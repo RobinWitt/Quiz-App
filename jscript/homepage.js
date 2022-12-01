08a825a6e9d9f0cce1baedf74361cbc141d61045
Robin Witt <robin.w@posteo.de>
2022-12-01 14:39:01 +0100

    change bookmark function

--- jscript/homepage.js
+++ jscript/homepage.js
@@ -1,36 +1,24 @@
 /* toggle bookmark */
 
-let svgPath = document.querySelector('[data-js="svgPath"]');
+const bookmarkIcon = document.querySelector('[data-js="bookmark__svg-path"]');
+const bookmarkIcon2 = document.querySelector('[data-js="bookmark__svg-path2"]');
 
 function toggleBookmark() {
-  if (
-    svgPath.getAttribute("d") ==
-    "M17,18L12,15.82L7,18V5H17M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z"
-  ) {
-    svgPath.setAttribute(
-      "d",
-      "M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z"
-    );
-    console.log("if works");
+  if (bookmarkIcon.getAttribute("fill") === "currentColor") {
+    bookmarkIcon.setAttribute("fill", "transparent");
+    console.log("unbookmarked");
   } else {
-    console.log("else works");
-    svgPath.setAttribute(
-      "d",
-      "M17,18L12,15.82L7,18V5H17M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z"
-    );
+    console.log("bookmarked");
+    bookmarkIcon.setAttribute("fill", "currentColor");
   }
 }
 
-/*
-const svgDisplay = document.querySelectorAll('[data-js="svg"]');
-
-console.log(svgDisplay);
-
-function toggleBookmark() {
-  if (document.getElementsByClassName("bookmark__svg") == "hidden") {
-    svgDisplay.classList.remove("hidden");
+function toggleBookmark2() {
+  if (bookmarkIcon2.getAttribute("fill") === "currentColor") {
+    bookmarkIcon2.setAttribute("fill", "transparent");
+    console.log("unbookmarked");
   } else {
-    svgDisplay.classList.add("hidden");
+    console.log("bookmarked");
+    bookmarkIcon2.setAttribute("fill", "currentColor");
   }
 }
-*/
