ef1a059ff41d26d314b7631f5a7a0b51731f991d
Robin Witt <robin.w@posteo.de>
2022-12-02 18:38:07 +0100

    create elements when submit

--- jscript/addnew.js
+++ jscript/addnew.js
@@ -11,34 +11,73 @@ createNewCardForm.addEventListener("submit", (event) => {
   // get form data
   const newQuestionData = new FormData(event.target);
   const newQuestionEntry = Object.fromEntries(newQuestionData);
-
-  // enter new data in elements
-  const cardNumber = document.querySelector(
-    '[data-js="question-card__number"]'
+  // create new question card
+  const newArticle = document.createElement("article");
+  newArticle.classList.add("question-card");
+  newCardDisplay.append(newArticle);
+  // create new question card header
+  const newHeader = document.createElement("nav");
+  newHeader.classList.add("question-card__header");
+  newArticle.append(newHeader);
+  // create card number
+  const newCardNumber = document.createElement("h2");
+  newCardNumber.classList.add("question-card__number");
+  newCardNumber.textContent = "13";
+  newHeader.append(newCardNumber);
+  // create new category list
+  const newCardCategoryList = document.createElement("ul");
+  newCardCategoryList.classList.add("question-card__categories");
+  newHeader.append(newCardCategoryList);
+  // create new category tag
+  const newCardCategoryTag = document.createElement("li");
+  newCardCategoryTag.classList.add("question-card__categories-tags");
+  newCardCategoryTag.textContent = newQuestionEntry.newCardCategory;
+  newCardCategoryList.append(newCardCategoryTag);
+  // create new bookmark icon
+  const newBookmarkButton = document.createElement("button");
+  newBookmarkButton.classList.add("bookmark");
+  newBookmarkButton.setAttribute("aria-label", "click to bookmark");
+  newBookmarkButton.setAttribute("data-js", "toggle-bookmark");
+  newHeader.append(newBookmarkButton);
+  // create new svg
+  const newSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  newSVG.classList.add("bookmark__svg");
+  newSVG.setAttribute("viewBox", "0 0 24 24");
+  newBookmarkButton.append(newSVG);
+  //create new svg path
+  const newSVGPath = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
   );
-  cardNumber.textContent = "13";
-  //add card categories
-  const category = document.querySelector(
-    '[data-js="question-card__categories-tags"]'
+  newSVGPath.setAttribute("data-js", "bookmark__svg-path");
+  newSVGPath.setAttribute("fill", "transparent");
+  newSVGPath.setAttribute("stroke", "var(--color-accent");
+  newSVGPath.setAttribute(
+    "d",
+    "M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z"
   );
-  category.textContent = newQuestionEntry.newCardCategory;
-  // create new question text
-  const question = document.querySelector(
-    '[data-js="question-card__question"]'
-  );
-  question.textContent = newQuestionEntry.newCardQuestion;
+  newSVG.append(newSVGPath);
+  // create new question title
+  const newQuestion = document.createElement("h3");
+  newQuestion.textContent = newQuestionEntry.newCardQuestion;
+  newArticle.append(newQuestion);
+  // create new show answer button
+  const newAnswerButton = document.createElement("button");
+  newAnswerButton.classList.add("question-card__button-show-answer");
+  newAnswerButton.setAttribute("data-js", "question-card__button-show-answer");
+  newAnswerButton.textContent = "Show Answer";
+  newArticle.append(newAnswerButton);
   // create new answer text
-  const answer = document.querySelector('[data-js="question-card__answer"]');
-  answer.textContent = newQuestionEntry.newCardAnswer;
-
-  // make article visible
-  questionCardArticle.style.display = "flex";
-
+  const newAnswerText = document.createElement("p");
+  newAnswerText.classList.add("question-card__answer");
+  newAnswerText.setAttribute("data-js", "question-card__answer");
+  newAnswerText.textContent = newQuestionEntry.newCardAnswer;
+  newArticle.append(newAnswerText);
   // clone article and make it visible
   /*
   const clone = questionCardArticle.cloneNode(true);
   newCardDisplay.append(clone);
-  clone.style.display = "block";
+  clone.style.display = "flex";
   console.log(clone);
   */
 
