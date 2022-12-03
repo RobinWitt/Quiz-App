35d935041ecc37cbd78961c0b5261b7e18bd414e
Robin Witt <robin.w@posteo.de>
2022-12-03 02:12:34 +0100

    login form slider

--- jscript/index.js
+++ jscript/index.js
@@ -1,24 +1,32 @@
-// create new account with email toggle
+// get login form data
+const loginForm = document.querySelector('[data-js="login-form"]');
 
-const createAccountButton = document.querySelector(
-  '[data-js="create-account-button"]'
-);
-const createAccountEmail = document.querySelector(
-  '[data-js="create-account-email"]'
-);
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-createAccountButton.addEventListener("click", () => {
-  const emailInput = document.querySelectorAll(
-    '[data-js="create-account-email"]'
+  const loginUserName = event.target.elements.username.value;
+  const loginPassword = event.target.elements.password.value;
+  console.log(loginUserName, loginPassword);
+
+  // event.target.reset();
+  // event.target.username.focus();
+});
+
+// create new account toggle
+
+const switchButton = document.querySelector('[data-js="switch"]');
+
+switchButton.addEventListener("click", () => {
+  const loginSection = document.querySelector('[data-js="login-section"]');
+  const createAccountSection = document.querySelector(
+    '[data-js="create-account-section"]'
   );
-  if (createAccountEmail.style.display === "flex") {
-    emailInput.forEach((element) => {
-      element.style.display = "none";
-    });
+  if (loginSection.style.top === "-500px") {
+    loginSection.style.top = "70px";
+    createAccountSection.style.top = "-500px";
   } else {
-    emailInput.forEach((element) => {
-      element.style.display = "flex";
-    });
+    loginSection.style.top = "-500px";
+    createAccountSection.style.top = "70px";
   }
 });
 
@@ -30,5 +38,5 @@ const resetPasswordButton = document.querySelector(
 const emailSentNotify = document.querySelector('[data-js="reset-notify"]');
 resetPasswordButton.addEventListener("click", () => {
   emailSentNotify.style.display = "block";
-  alert("We sent you an email to reset your password.");
+  // alert("We sent you an email to reset your password.");
 });
