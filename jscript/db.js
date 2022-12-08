2e22a178f9297da75e016592657d946e8b35336f
Robin Witt <robin.w@posteo.de>
2022-12-08 19:40:04 +0100

    create db js add content

---
+++ jscript/db.js
@@ -0,0 +1,24 @@
+export const questionAPI = [
+  {
+    question: "What does html stand for?",
+    categories: ["html", "web", "coding"],
+    answer: "html stands for Hyper Text Markup Language",
+  },
+  {
+    question: "What does css stand for?",
+    categories: ["css", "web", "coding"],
+    answer: "css stands for Cascading Style Sheets",
+  },
+  {
+    question: "How much strings does a standard guitar have?",
+    categories: ["instruments", "guitar", "music"],
+    answer: "Six strings",
+  },
+  {
+    question: "How many months are in a year?",
+    categories: ["time", "calendar"],
+    answer: "One year consists of 12 months",
+  },
+];
+
+console.log(questionAPI);
